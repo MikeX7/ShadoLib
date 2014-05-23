f4d0b52e053511ef7fe21c90354c0bf675b5eeb5
Mike <imike@centrum.cz>
2014-05-23 11:22:46 +0200

    Added ResourceExists function

--- ShadoLib/Tools.cs
+++ ShadoLib/Tools.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text.RegularExpressions;
 
 namespace ShadoLib
 {
-    static public class Tools
+    public static class Tools
     {
         public static String BytesFormat(ulong bytes)
-        {           
+        {
             if (bytes >= 1125899906842624)
-                return ((double)bytes / 1125899906842624).ToString("0.##") + "PB";
+                return ((double) bytes/1125899906842624).ToString("0.##") + " PB";
             else if (bytes >= 1099511627776)
-                return ((double)bytes / 1099511627776).ToString("0.##") + "TB";
+                return ((double) bytes/1099511627776).ToString("0.##") + " TB";
             else if (bytes >= 1073741824)
-                return ((double)bytes / 1073741824).ToString("0.##") + "GB";
+                return ((double) bytes/1073741824).ToString("0.##") + " GB";
             else if (bytes >= 1048576)
-                return ((double)bytes / 1048576).ToString("0.##") + "MB";
+                return ((double) bytes/1048576).ToString("0.#") + " MB";
             else if (bytes >= 1024)
-                return ((double)bytes / 1024).ToString("0.##") + "KB";
+                return ((double) bytes/1024).ToString("0") + " KB";
             else
-                return bytes + "B";
+                return bytes + " B";
 
-            
+
         }
 
         public static Hashtable ArgCutter(String[] args)
@@ -33,11 +38,29 @@ namespace ShadoLib
 
             var argTab = new Hashtable();
 
-            foreach (Match m in mc)            
+            foreach (Match m in mc)
                 argTab.Add(m.Groups[1].ToString(), m.Groups[2].ToString());
-             
+
             return argTab;
         }
+
+        public static bool ResourceExists(string path, Assembly assembly)
+        {           
+            using (var stream = assembly.GetManifestResourceStream(assembly.GetName().Name + ".g.resources"))
+            {
+                if (stream == null) 
+                    return false;
+
+                using (var reader = new System.Resources.ResourceReader(stream))
+                {
+                    var dict = reader.Cast<DictionaryEntry>().Select(entry =>
+                        (string)entry.Key).ToArray();
+
+                    return dict.Contains(path);
+                }
+            }            
+        }
+        
     }
 }
 
